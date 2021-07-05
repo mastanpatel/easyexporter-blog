da48e31990f87035e4fa150679557be2870b39a4
mastan patel <m.mastanpatel@gmail.com>
2021-07-05 23:28:43 +0530

    added social icons and navbar

--- eeblog/src/App.js
+++ eeblog/src/App.js
@@ -3,11 +3,12 @@ import Home from './components/Home';
 import About from './components/About';
 import SinglePost  from './components/SinglePost';
 import Post from './components/Post';
-
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <BrowserRouter>
+    <NavBar />
       <Switch>
         <Route component={Home} path='/'  exact/>
         <Route component={About} path='/about' />
--- eeblog/src/components/NavBar.js
+++ eeblog/src/components/NavBar.js
@@ -1,5 +1,59 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 export default function NavBar() {
-    return <h1>NavBar Page!</h1>
+  return (
+    <header className="bg-red-600">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink
+            to="/"
+            exact
+            activeClassName="text-white"
+            className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
+          >
+            EasyExporter
+          </NavLink>
+          <NavLink
+            to="/post"
+            className="inline-flex item-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+            activeClassName="text-red-100 bg-red-700"
+          >
+            Blog
+          </NavLink>
+          <NavLink
+            to="/About"
+            className="inline-flex item-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+            activeClassName="text-red-100 bg-red-700"
+          >
+            About Us!
+          </NavLink>
+        </nav>
+        <div className="inline-flex py-3 px-3 my-6">
+          <SocialIcon
+            url="https://www.youtube.com/channel/UClYs40syVBpxezvSe3RiCrA"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://www.linkedin.com/company/66748837/"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://www.facebook.com/mastan.patel.5"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+        </div>
+      </div>
+    </header>
+  );
 }
