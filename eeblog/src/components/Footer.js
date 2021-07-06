8ebb8f88cb6c42e580775e83eb1a8b706a508f49
mastan patel <m.mastanpatel@gmail.com>
2021-07-06 09:24:38 +0530

    added single post and about page

--- eeblog/src/components/Footer.js
+++ eeblog/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <footer className="bg-green-600 text-center fixed inset-x-0 bottom-0">
-      <div className="container mx-auto flex justify-between">
+      <div className="container mx-auto flex justify-center">
         <div className="inflex-flex items-center py-6 px-3 mr-4 text-green-100 hover:text-red-800 text-1xl font-bold cursive tracking-widest">
           Copyright © {new Date().getFullYear()}
           <span> All Rights Reserved</span>
