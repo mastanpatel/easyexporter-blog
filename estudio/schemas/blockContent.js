86f6ac44c4d8fbebc0504c565b659f99443324fe
Mastan Patel <m.mastanpatel@gmail.com>
2021-08-10 06:16:09 +0530

    changes on 10 auugst

--- estudio/schemas/blockContent.js
+++ estudio/schemas/blockContent.js
@@ -26,6 +26,7 @@ export default {
         {title: 'H2', value: 'h2'},
         {title: 'H3', value: 'h3'},
         {title: 'H4', value: 'h4'},
+        {title: 'P', value:'p'},
         {title: 'Quote', value: 'blockquote'},
       ],
       lists: [{title: 'Bullet', value: 'bullet'}],
