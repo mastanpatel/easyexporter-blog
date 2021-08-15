e5cf42ee580439e1c0fea32978b041bc96a1467d
Mastan Patel <m.mastanpatel@gmail.com>
2021-08-15 17:25:50 +0530

    added google analytics tracking

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import SinglePost from "./components/SinglePost";
 import Post from "./components/Post";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import ReactGA from 'react-ga';
+
+
+ReactGA.initialize('UA-204991172-1 ');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
